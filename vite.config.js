e3e242f735de744f5f81af3fe2c2c6a91569e5b8
joan <engonej2@gmail.com>
2025-07-16 13:31:38 +0200

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react(),    // Add React plugin here
-    tailwindcss()
-  ],
+  plugins: [react()],
+  base: process.env.VITE_BASE_PATH || '/agneghe',
 });
