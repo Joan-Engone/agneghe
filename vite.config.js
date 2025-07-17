363afc3170c1a1877a22ad8a704763cfabde09ad
joan <engonej2@gmail.com>
2025-07-17 02:20:28 +0200

    Fix Vite base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: mode === 'development' ? '/' : '/agneghe',
-}));
+  base: './', // ✅ Ensures relative paths work in Vercel build
+});
